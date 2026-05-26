231f1c26c71fa84795d842281b0f52564037ad75
Deepak Kumar <deepak0422v@gmail.com>
2026-05-26 16:52:25 +0530

    Simplify vite config to supported APIs

--- frontend2/vite.config.ts
+++ frontend2/vite.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    tsConfigPaths(),
+    tanstackStart(),
     react(),
-    tanstackStart({
-      customViteReactPlugin: true,
-      server: {
-        entry: "./src/server.ts",
-      },
-    }),
+    tsConfigPaths(),
   ],
 });
