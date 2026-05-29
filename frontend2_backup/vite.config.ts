db4b54341202db329f118fd892368102a99edac4
Deepak Kumar <deepak0422v@gmail.com>
2026-05-29 12:26:52 +0530

    Stable Lumora backend

--- frontend2_backup/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
-export default defineConfig({
-  plugins: [
-    tanstackStart(),
-  ],
-});
